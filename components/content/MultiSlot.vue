d955dcdbc79cb96156dbae95147ed6fd276d21f1
Farnabaz <farnabaz@gmail.com>
2024-03-20 12:32:21 +0100

    Create MultiSlot.vue

---
+++ components/content/MultiSlot.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="gray_box">
+        <p class="title">Performance standard</p>
+        <p class="small"><slot name="standard"  /></p>
+    </div>
+   
+    <div class="columns">
+        <div class="gray_box">
+            <p class="title">Related concepts</p>
+            <p class="small"><slot name="concepts"  /></p>
+        </div>
+        <div class="gray_box">
+            <p class="title">MiR Academy activities</p>
+            <p class="small"><slot name="activities"  /></p>
+        </div>
+
+
+    </div>
+
+</template>
